a95fae3c9428c923bf342cc079d8acca4bdeaddd
karis <rose_redk_@hotmail.com>
2019-03-05 13:31:28 -0600

    keyboard

--- src/app/pantallas/referencia/referencia.component.ts
+++ src/app/pantallas/referencia/referencia.component.ts
@@ -1,23 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
 import { AppService } from 'src/app/shared/app.service';
+import Keyboard from "simple-keyboard";
 
 @Component({
   selector: 'app-referencia',
   templateUrl: './referencia.component.html',
-  styleUrls: ['./referencia.component.css']
-  
+  styleUrls: [    
+    './referencia.component.css']  
 })
 export class ReferenciaComponent implements OnInit {
   servicioId: number;
+  value = "";
+  keyboard: Keyboard;
+
 
   constructor(private appService:AppService) { }
 
   ngOnInit() {
     this.servicioId = this.appService.global.servicioId;
+    this.keyboard = new Keyboard({
+      onChange: input => this.onChange(input),
+      onKeyPress: button => this.onKeyPress(button)
+    });
   }
 
   establecerReferencia(ref:string){
-    console.log(ref);    
     this.appService.global.referencia = ref;
   }
+
+  onChange = (input: string) => {
+    this.value = input;
+    this.establecerReferencia(input)
+  };
+
+  onKeyPress = (button: string) => {
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+  };
+
+  onInputChange = (event: any) => {
+    this.keyboard.setInput(event.target.value);  
+
+  };
+
+  handleShift = () => {
+    let currentLayout = this.keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    this.keyboard.setOptions({
+      layoutName: shiftToggle
+    });
+  };
 }
